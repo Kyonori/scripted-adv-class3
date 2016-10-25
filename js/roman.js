0b29b7a11699b605bc2af59d6fad5f4098f7fba3
kyonori <rickniigataneko@gmail.com>
2016-10-25 22:00:03 +0000

    my comment

--- js/roman.js
+++ js/roman.js
@@ -1,42 +1,101 @@
 $(function() {
-  console.log("JavaScript is working on this site! Cool!");
+      console.log("JavaScript is working on this site! Cool!");
 
 
-  //-----------------------------------------------------------------------------------
-  // convertToRomanNumeral function takes number as a parameter 
-  // and returns a Roman numeral
-  // 
-  // 100 | C 
-  // 90  | XC
-  // 50  | L
-  // 40  | XL
-  // 10  | X
-  // 9   | IX
-  // 5   | V
-  // 4   | IV
-  // 1   | I
+      //-----------------------------------------------------------------------------------
+      // convertToRomanNumeral function takes number as a parameter 
+      // and returns a Roman numeral
+      // 
+      // 100 | C 
+      // 90  | XC
+      // 50  | L
+      // 40  | XL
+      // 10  | X
+      // 9   | IX
+      // 5   | V
+      // 4   | IV
+      // 1   | I
 
-  function convertToRomanNumeral(number) {
-    console.log("Number = " + number);
-    var output = "";
-    return output;
-  }
+      function convertToRomanNumeral(input) {
+
+        var output = "";
+        
+        // if (input == 0) {
+        //   output=output.concat("nulla")
+        // }
+        // else if ($.isNumeric(input))
+        
+        
+        while (input > 0) {
+          if (input >= 100) {
+            output=output.concat("C");
+            input -= 100;
+
+
+          }
+          else if (input >= 90) {
+            output=output.concat("XC");
+            input -= 90;
+          }
+          else if (input >= 50) {
+            output=output.concat("L");
+            input -= 50;
+          }
+          else if (input >= 40) {
+            output=output.concat("XL");
+            input -= 40;
+          }
+          else if (input >= 10) {
+            output=output.concat("X");
+            input -= 10;
+          }
+          else if (input >= 9) {
+            output=output.concat("IX");
+            input -= 9;
+          }
+          else if (input >= 5) {
+            output=output.concat("V");
+            input -= 5;
+          }
+          else if (input >= 4) {
+            output=output.concat("IV");
+            input -= 4;
+          }
+          else if (input >= 1) {
+            output=output.concat("I");
+            input -= 1;
+          }
+          else if (input == 0) {
+            output=output.concat("nulla")
+          }
+          else {
+            output="This is not a roman numeral"
+          }
+
+          }
 
 
 
-  //-----------------------------------------------------------------------------------
-  // Convert the number input when the button is clicked
 
-  var $button = $("button");
-  var $numberInput = $("#number_input");
-  var $romanNumeralOutput = $("#roman_numeral_output");
+          console.log("Number = " + input);
+          return output;
+        }
 
-  $button.on("click", function(e){
-    e.preventDefault();
-    numberVal = $numberInput.val();
-    romanNumeralVal = convertToRomanNumeral(numberVal);
-    console.log("Roman numeral = " + romanNumeralVal);
-    $romanNumeralOutput.val(romanNumeralVal);
-  });
 
-});
+
+        //-----------------------------------------------------------------------------------
+        // Convert the number input when the button is clicked
+
+        var $button = $("button");
+        var $numberInput = $("#number_input");
+        var $romanNumeralOutput = $("#roman_numeral_output");
+
+        $button.on("click", function(e) {
+          e.preventDefault();
+          numberVal = $numberInput.val();
+          romanNumeralVal = convertToRomanNumeral(numberVal);
+          console.log("Roman numeral = " + romanNumeralVal);
+          $romanNumeralOutput.val(romanNumeralVal);
+        });
+
+      });
